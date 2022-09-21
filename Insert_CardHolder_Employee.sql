dbab43f7ce2bd4a05d3273f64b89bdc88c947d9c
JosephAMay <66848177+JosephAMay@users.noreply.github.com>
2022-09-21 13:29:42 -0500

    Add files via upload

---
+++ Insert_CardHolder_Employee.sql
@@ -0,0 +1,28 @@
+use libraryDB;
+show tables;
+
+
+insert into cardholder values('Joseph', 'March', '12345', '2022-09-21', '0', 0.0, '1234 Here Street', '111111111');
+insert into cardholder values('Greg', 'LastName', '54321', '2022-09-22','1', 12, '200 Blah Blah Avenue','123456789');
+insert into cardholder values('Rachel', 'Brown', '11111', '2000-01-01', '0', 0, '4321 Street Boulevard', '111111111');
+insert into cardholder values('Crystal', 'Peirce', '22222', '2001-02-22', '1', '10','932 Fake Street APT 22', '111111111');
+insert into cardholder values('Snoop', 'Dogg', '33333', '2021-04-20','0','0','Big Mansion Hollywood, CA','123456789');
+insert into cardholder values('GI', 'Joe', '55555', '1930-03-03', '0', '0', 'Joe HQ', '111111111');
+insert into cardholder values('Clark', 'Kent', '44444', '1941-01-03', '0', '0', 'Fortress of Solitude', '111111111');
+insert into cardholder values('Albert', 'Einstein', '12222', '1903-01-03', '1', '200.0', '1212 science road', '111111111');
+insert into cardholder values('Rudolph', 'Red Nose Reindeer', '11222', '1800-12-26', '0', '0', 'North Pole', '111111111');
+insert into cardholder values('Freddie', 'Mercury', '11122', '1957-08-08', '0', '0', 'Big Mansion London', '111111111');
+
+#select * from cardholder;
+
+insert into employee values('Jeremy', 'Fisk', 'N', 35000, '222222222', '222222222', '234 Library road', '111111111');
+insert into employee values('Danielle', 'Rose', 'A', 35000, '222222222', '223344551', '321 Howard Lane', '111111111');
+insert into employee values('Lori', 'Gonzales', 'K', 55000, '222222222', '112233445', '3708 Lake Crossing Road', '123456789');
+insert into employee values('Levi', 'McDonald', 'O', 22000, '112233445', '332211441', '764 Union Avenue', '123456789');
+insert into employee values('Mario', 'Smith', 'w', 33000, '112233445', '999888777','0998 Small Loop Lane', '123456789');
+insert into employee values('Water' ,'Bottle', 'H', 60000, '444555666', '444555666', '3232 Pensylvanie Ave', '987654321');
+insert into employee values('Nathan', 'Jones', 'P', 55000, '444555666', '321321321', '233 Michigan Ave', '987654321');
+insert into employee values('Kacey', 'Gold' , 'G', 32000, '222222222', '454545459', '543 Big Hill Road', '111111111');
+insert into employee values('Joshua', 'Caroll', 'D', 25000, '454545459', '111111112', '8745 Small Mountain drive', '111111111');
+
+#select * from employee;
