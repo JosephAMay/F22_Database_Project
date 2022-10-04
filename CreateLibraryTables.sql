cdb49d66bf87c380015f70a619915110b171ded9
JosephAMay <66848177+JosephAMay@users.noreply.github.com>
2022-10-03 20:47:51 -0500

    Update CreateLibraryTables.sql

--- CreateLibraryTables.sql
+++ CreateLibraryTables.sql
@@ -5,33 +5,37 @@ show tables;
 
 create table library
 (
-	Locationname		VARCHAR(50)	NOT NULL,
-    address				VARCHAR(50) NOT NULL,
+	Locationname		VARCHAR(100)	NOT NULL,
+    address				VARCHAR(100) NOT NULL,
     libraryID			Char(9) NOT NULL,
-    membershipFee		Decimal(10,2),
+    membershipFee		Decimal(10,2) default 0,
 	PRIMARY KEY(libraryID)
 
 );
+
+#alter table booklist
+#drop column pub_date;
+
 create table booklist
 (
-	Auth_Fname 		varchar (20), 
-    Auth_Lname 		varchar (20), 
-	publisher		varchar (35) NOT NULL,
-	title			varchar(50) NOT NULL,
+	Auth_Fname 		varchar (100), 
+    Auth_Lname 		varchar (100), 
+	publisher		varchar (100) NOT NULL,
+	title			varchar(100) NOT NULL,
 	num_copies      int NOT NULL,
 	housed_in		char(9) NOT NULL,
 	ISBN			char(13) NOT NULL,
-	genre			varchar(20),
-	sub_genre		varchar(20),
+	genre			varchar(100),
+	sub_genre		varchar(100) default 'N/A',
     
     primary key(ISBN, housed_in),
     foreign Key(housed_in) references library(libraryID)
 );
 create table movielist
 (
-	title			varchar(50) not null,
-	genre           varchar(30),
-    studio			varchar(30),
+	title			varchar(100) not null,
+	genre           varchar(100),
+    studio			varchar(100),
 	rating			float(10,2),
 	profit			float(10,2),
     release_date	char(4) not null,
@@ -40,34 +44,37 @@ create table movielist
     primary key(title, release_date),
     foreign key(housed_in) references library(libraryID)
 );
+
 create table audiolist
 (
 	release_date	date not null,
 	album			varchar(100),
-	artist			varchar(60) not null,
-	genre			varchar(60),
-	sub_genre		varchar(60),
+	artist			varchar(100) not null,
+	genre			varchar(100),
+	sub_genre		varchar(100),
     housed_in		char(9) not null,
     num_copies		int not null,
     primary key (artist, album),
     foreign key(housed_in) references library(libraryID)
 );
+
 create table cardholder
 (
 	 Fname			varchar(20) not null,	
 	 Lname			varchar(20) not null,	
-	 cardID			char(5) not null,
-	 signup_date	        date,
-	 overdue		char(1) not null,
-	 fees			decimal(10,2),
+	 cardID			int AUTO_INCREMENT,
+	 signup_date	date DEFAULT '2022-10-03',
+	 overdue		char(1) not null default 0,
+	 fees			decimal(10,2) default 0,
 	 address		varchar(30) not null,
 	 member_of		char(9) not null,
-	 username               varchar(30),
-     	 password               varchar(20),
+     username      varchar(30) default null,
+     password       varchar(20) default null,
      
 	primary key (cardID),
     foreign key(member_of) references library(libraryID)
 );
+alter table cardholder auto_increment = 0;
 create table employee
 (
 	Fname			varchar(25) not null,
@@ -78,22 +85,25 @@ create table employee
     ssn				char(9) not null,
     home_address	varchar(35) not null,
     workLocation	char(9) not null,
+    username      varchar(30) default null,
+     password       varchar(20) default null,
+    
     
     primary key (ssn),
     foreign key (workLocation) references library(libraryID)
 );
 create table onloan
 (
-    cardID          char(5) not null,
+    cardID          int,
     housed_in       char(9) not null,
-    bookTitle       varchar(50),
-    ISBN            char(13),
-    movie_Title     varchar(30),
-    movie_release   char(4),
-    artistName      varchar(30),
-    albumName       varchar(20),
-	backBy          date,
-    transactionNum  char(10) not null,
+    bookTitle       varchar(100) default null,
+    ISBN            char(13) default null,
+    movie_Title     varchar(100) default null,
+    movie_release   char(4) default null,
+    artistName      varchar(100) default null,
+    albumName       varchar(100) default null,
+	backBy          date DEFAULT '2022-10-03',
+    transactionNum  int auto_increment not null,
     
     primary key(transactionNum, cardID),
     foreign key(cardID) references cardholder(cardID),
@@ -104,9 +114,9 @@ create table onloan
 );
 create table returned
 (
-	cardID				char(5) not null,
+	cardID				int,
     date_Returned		date not null,
-	transactionNum		char(12) not null,
+	transactionNum		int not null,
     
     primary key(cardID, transactionNum),
     foreign key(cardID) references cardholder(cardID),
