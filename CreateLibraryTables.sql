6af4807dfb10dde88557f6f62ba280d4fd01f977
JosephAMay <66848177+JosephAMay@users.noreply.github.com>
2022-10-12 20:10:09 -0500

    Update CreateLibraryTables.sql

--- CreateLibraryTables.sql
+++ CreateLibraryTables.sql
@@ -8,7 +8,7 @@ create table library
 	Locationname		VARCHAR(100)	NOT NULL,
     address				VARCHAR(100) NOT NULL,
     libraryID			Char(9) NOT NULL,
-    membershipFee		Decimal(10,2) default 0,
+    membershipFee		Decimal(10,2) default 0 check(membershipFee >=0),
 	PRIMARY KEY(libraryID)
 
 );
@@ -22,7 +22,7 @@ create table booklist
     Auth_Lname 		varchar (100), 
 	publisher		varchar (100) NOT NULL,
 	title			varchar(100) NOT NULL,
-	num_copies      int NOT NULL,
+	num_copies      int NOT NULL check(num_copies >= 0),
 	housed_in		char(9) NOT NULL,
 	ISBN			char(13) NOT NULL,
 	genre			varchar(100),
@@ -36,11 +36,11 @@ create table movielist
 	title			varchar(100) not null,
 	genre           varchar(100),
     studio			varchar(100),
-	rating			float(10,2),
+	rating			float(10,2) check(rating >= 0),
 	profit			float(10,2),
     release_date	char(4) not null,
     housed_in		char(9) not null,
-    num_copies      int not null,
+    num_copies      int not null check(num_copies >=0),
     primary key(title, release_date),
     foreign key(housed_in) references library(libraryID)
 );
@@ -53,7 +53,7 @@ create table audiolist
 	genre			varchar(100),
 	sub_genre		varchar(100),
     housed_in		char(9) not null,
-    num_copies		int not null,
+    num_copies		int not null check(num_copies >=0),
     primary key (artist, album),
     foreign key(housed_in) references library(libraryID)
 );
@@ -65,7 +65,7 @@ create table cardholder
 	 cardID			int AUTO_INCREMENT,
 	 signup_date	date DEFAULT '2022-10-03',
 	 overdue		char(1) not null default 0,
-	 fees			decimal(10,2) default 0,
+	 fees			decimal(10,2) default 0 check (fees >=0),
 	 address		varchar(30) not null,
 	 member_of		char(9) not null,
      username      varchar(30) default null,
@@ -80,7 +80,7 @@ create table employee
 	Fname			varchar(25) not null,
     Lname			varchar(25) not null,
     Minit			char(1),
-    salary			decimal(10,2),
+    salary			decimal(10,2) check(salary >= 0 and salary <=250000),
     managed_by		char(9) not null,
     ssn				char(9) not null,
     home_address	varchar(35) not null,
@@ -91,6 +91,7 @@ create table employee
     
     primary key (ssn),
     foreign key (workLocation) references library(libraryID)
+    
 );
 create table onloan
 (
@@ -122,3 +123,16 @@ create table returned
     foreign key(cardID) references cardholder(cardID),
     foreign key(transactionNum) references onloan(transactionNum)
 );
+
+
+#check when return date is past current date, add fee to cardholder, set  overdue to 1
+
+#If all tems that are  overdue are returned, set overdue to 0 for cardholder
+
+#make salary of employee less than manager
+
+#when a book is removed from returned, add to num copies of that item in library it belongs
+
+#If item in onLoan for an item that has 0 num copies, abort transaction
+
+#On update, on delete? 
